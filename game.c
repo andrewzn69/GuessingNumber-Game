ef889188744ce35cc4675c69008bc91e83d34da6
andrewzn69 <andrewzn23@gmail.com>
2022-03-19 21:35:10 +0100

    pomoc

--- game.c
+++ game.c
@@ -1,11 +1,11 @@
 /***************************************************************
  *
- *   Program: Hadani nahodnych cisel
+ *   Program: Hadani nahodeho cisla
  *
  *   Autor: Ondrej Zeman, IT1B
  *
  *   Description: Uzivatel si vybere obtiznost, nasledne podle
- *                vyberu hada nahodne vygenerovane cisla
+ *                vyberu hada nahodne vygenerovane cislo
  *
  ***************************************************************/
 
@@ -16,6 +16,10 @@
 // Bolest prstu ✨ gone ✨
 #define p printf
 
+// Promenna pro zapnuti napovedy, jeji globalita je potrebna pro ulozeni preference pro opakovani hry
+
+char napoveda = 'N';
+
 int main()
 {
     system("cls");
@@ -24,11 +28,11 @@ int main()
     srand(time(0));
 
     // Deklarujeme si promenne
-    int lives, randomnum, range1, range2, guess, vyber_menu = 0, vyber_nastaveni = 1;
-    char napoveda = 'N', repeat;
+    int lives, randomnum, range1, range2, guess, vyber_nastaveni, zadana_hodnota = 0, vyber_menu = 0;
+    char repeat = 'n';
 
     // Uzivatel si vybere obtiznost, nebo vypne hru, (loop pro blbuvzdornost)
-    while (vyber_menu < 1 || vyber_menu > 7 || vyber_nastaveni == 2 || vyber_nastaveni == 0 || repeat == 'y' || repeat == 'Y')
+    while (vyber_menu < 1 || vyber_menu > 6)
     {
         p("[1] Lekha - rozmezi 1-10, 4 pokusy\n");
         p("[2] Stredni - rozmezi 1-50, 7 pokusu\n");
@@ -47,7 +51,7 @@ int main()
         case 1:
             lives = 4;
             range1 = 1;
-            range2 = 1;
+            range2 = 10;
             randomnum = rand() % range2 + range1;
             break;
 
@@ -70,13 +74,18 @@ int main()
         // Popis hry uzivatelovi
         case 4:
             system("cls");
-            p("V teto hre musis uhodnout dany pocet nahodne generovanych cisel.\n");
-            p("Pro START zmackni [ENTER]\n");
+            p("V teto hre musis uhodnout nahodne vygenerovane cislo\n");
+            p("Pro navrat do menu zmackni [ENTER]\n\n");
+            getchar();
             getchar();
             system("cls");
+            main();
+            break;
 
         // Uzivatel si muze zapnout napovedu, defaultne je napoveda vypnuta
         case 5:
+
+            // Jestli uzivatel nevybere navrat do menu
             while (vyber_nastaveni != 2)
             {
                 p("[1] Napoveda: %c\n", napoveda);
@@ -90,6 +99,7 @@ int main()
                     napoveda = 'N';
                 system("cls");
             }
+            vyber_menu = 0;
             vyber_nastaveni = 0;
             break;
 
@@ -98,22 +108,72 @@ int main()
             system("cls");
             exit(0);
             break;
-        }
-
-        // Hra zacina: Uzivatel hada cislo, ukazuji se mu zivoty a rozmezi cisel
-        while (guess != randomnum && lives > 0)
-        {
-            p("Uhodni cislo v rozmezi %d-%d, zbyva ti %d zivotu\n\n", range1, range2, lives);
-            scanf("%d", &guess);
-
-            if (guess == randomnum)
-            {
-                system("cls");
-                p("Krasna prace, kamarade, chces to zkusit znovu? (y/n)");
-                scanf(" %c", &repeat);
-                system("cls");
-            }
+        default:
+            main();
         }
     }
-    repeat = 'n';
+
+    // Zacatek hry, uzivatel hada vygenerovane cislo
+    while (guess != randomnum && lives >= 1)
+    {
+        // Napoveda zapnuta
+        if (zadana_hodnota > 0)
+        {
+            if (napoveda == 'Y' && guess < randomnum)
+                p("Cislo je vetsi!\n\n");
+            else if (napoveda == 'Y' && guess > randomnum)
+                p("Cislo je mensi!\n\n");
+        }
+
+        // Hlavni otazka
+        p("Uhodni cislo (%d-%d), zbyva ti %d pokusu.\n\n", range1, range2, lives);
+        scanf("%d", &guess);
+        zadana_hodnota++;
+        lives--;
+        system("cls");
+
+        // Uzivatel vyhraje
+        while (guess == randomnum)
+        {
+            p("Vyhral jsi!\n");
+
+            // Program se pta uzivatele, jestli chce opakovat hru
+            p("Chces hru opakovat znovu? (y/n)\n\n");
+            scanf(" %c", &repeat);
+            system("cls");
+
+            // Uzivatel chce opakovat hru
+            if (repeat == 'y' || repeat == 'Y')
+            {
+                main();
+            }
+
+            // Uzivatel nechce opakovat hru
+            else
+                exit(0);
+        }
+
+        // Uzivatel prohraje
+        while (lives == 0)
+        {
+            system("cls");
+            p("Prohral jsi!\n");
+            p("Cislo bylo: %d\n", randomnum);
+
+            // Program se pta uzivatele, jestli chce opakovat hru
+            p("Chces to opakovat znova? (y/n)\n\n");
+            scanf(" %c", &repeat);
+            system("cls");
+
+            // Uzivatel chce opakovat hru
+            if (repeat == 'y' || repeat == 'Y')
+            {
+                main();
+                vyber_menu = 0;
+            }
+            // Uzivatel nechce opakovat hru
+            else
+                exit(0);
+        }
+    }
 }
